c1ba6bb690876748c162793784bd22d156306ec0
per1234 <accounts@perglass.com>
2020-11-24 23:52:14 -0800

    Use project-type specific names in test helpers for library checks of checkfunctions package
    This will avoid name collisions with the similar helpers used in the tests for the check functions of other project
    types.

--- check/checkfunctions/library_test.go
+++ check/checkfunctions/library_test.go
@@ -38,14 +38,14 @@ func init() {
 	schemasPath = paths.New(workingDirectory, "..", "..", "etc", "schemas")
 }
 
-type checkFunctionTestTable struct {
+type libraryCheckFunctionTestTable struct {
 	testName            string
 	libraryFolderName   string
 	expectedCheckResult checkresult.Type
 	expectedOutputQuery string
 }
 
-func checkCheckFunction(checkFunction Type, testTables []checkFunctionTestTable, t *testing.T) {
+func checkLibraryCheckFunction(checkFunction Type, testTables []libraryCheckFunctionTestTable, t *testing.T) {
 	for _, testTable := range testTables {
 		expectedOutputRegexp := regexp.MustCompile(testTable.expectedOutputQuery)
 
@@ -64,34 +64,34 @@ func checkCheckFunction(checkFunction Type, testTables []checkFunctionTestTable,
 }
 
 func TestLibraryPropertiesNameFieldMissingOfficialPrefix(t *testing.T) {
-	testTables := []checkFunctionTestTable{
+	testTables := []libraryCheckFunctionTestTable{
 		{"Unable to load", "InvalidLibraryProperties", checkresult.NotRun, ""},
 		{"Not defined", "MissingFields", checkresult.NotRun, ""},
 		{"Correct prefix", "Arduino_Official", checkresult.Pass, ""},
 		{"Incorrect prefix", "Recursive", checkresult.Fail, "^Recursive$"},
 	}
 
-	checkCheckFunction(LibraryPropertiesNameFieldMissingOfficialPrefix, testTables, t)
+	checkLibraryCheckFunction(LibraryPropertiesNameFieldMissingOfficialPrefix, testTables, t)
 }
 
 func TestLibraryPropertiesNameFieldDuplicate(t *testing.T) {
-	testTables := []checkFunctionTestTable{
+	testTables := []libraryCheckFunctionTestTable{
 		{"Unable to load", "InvalidLibraryProperties", checkresult.NotRun, ""},
 		{"Duplicate", "Indexed", checkresult.Fail, "^Servo$"},
 		{"Not duplicate", "NotIndexed", checkresult.Pass, ""},
 	}
 
-	checkCheckFunction(LibraryPropertiesNameFieldDuplicate, testTables, t)
+	checkLibraryCheckFunction(LibraryPropertiesNameFieldDuplicate, testTables, t)
 }
 
 func TestLibraryPropertiesNameFieldNotInIndex(t *testing.T) {
-	testTables := []checkFunctionTestTable{
+	testTables := []libraryCheckFunctionTestTable{
 		{"Unable to load", "InvalidLibraryProperties", checkresult.NotRun, ""},
 		{"In index", "Indexed", checkresult.Pass, ""},
 		{"Not in index", "NotIndexed", checkresult.Fail, "^NotIndexed$"},
 	}
 
-	checkCheckFunction(LibraryPropertiesNameFieldNotInIndex, testTables, t)
+	checkLibraryCheckFunction(LibraryPropertiesNameFieldNotInIndex, testTables, t)
 }
 
 func TestLibraryPropertiesNameFieldHeaderMismatch(t *testing.T) {
@@ -105,38 +105,38 @@ func TestLibraryPropertiesNameFieldHeaderMismatch(t *testing.T) {
 }
 
 func TestLibraryPropertiesSentenceFieldSpellCheck(t *testing.T) {
-	testTables := []checkFunctionTestTable{
+	testTables := []libraryCheckFunctionTestTable{
 		{"Unable to load", "InvalidLibraryProperties", checkresult.NotRun, ""},
 		{"Not defined", "MissingFields", checkresult.NotRun, ""},
 		{"Misspelled word", "MisspelledSentenceParagraphValue", checkresult.Fail, "^grill broccoli now$"},
 		{"Correct spelling", "Recursive", checkresult.Pass, ""},
 	}
 
-	checkCheckFunction(LibraryPropertiesSentenceFieldSpellCheck, testTables, t)
+	checkLibraryCheckFunction(LibraryPropertiesSentenceFieldSpellCheck, testTables, t)
 }
 
 func TestLibraryPropertiesParagraphFieldSpellCheck(t *testing.T) {
-	testTables := []checkFunctionTestTable{
+	testTables := []libraryCheckFunctionTestTable{
 		{"Unable to load", "InvalidLibraryProperties", checkresult.NotRun, ""},
 		{"Not defined", "MissingFields", checkresult.NotRun, ""},
 		{"Misspelled word", "MisspelledSentenceParagraphValue", checkresult.Fail, "^There is a zebra$"},
 		{"Correct spelling", "Recursive", checkresult.Pass, ""},
 	}
 
-	checkCheckFunction(LibraryPropertiesParagraphFieldSpellCheck, testTables, t)
+	checkLibraryCheckFunction(LibraryPropertiesParagraphFieldSpellCheck, testTables, t)
 }
 
 func TestLibraryPropertiesParagraphFieldRepeatsSentence(t *testing.T) {
-	testTables := []checkFunctionTestTable{
+	testTables := []libraryCheckFunctionTestTable{
 		{"Unable to load", "InvalidLibraryProperties", checkresult.NotRun, ""},
 		{"Repeat", "ParagraphRepeatsSentence", checkresult.Fail, ""},
 		{"No repeat", "Recursive", checkresult.Pass, ""},
 	}
 
-	checkCheckFunction(LibraryPropertiesParagraphFieldRepeatsSentence, testTables, t)
+	checkLibraryCheckFunction(LibraryPropertiesParagraphFieldRepeatsSentence, testTables, t)
 }
 func TestLibraryPropertiesUrlFieldDeadLink(t *testing.T) {
-	testTables := []checkFunctionTestTable{
+	testTables := []libraryCheckFunctionTestTable{
 		{"Unable to load", "InvalidLibraryProperties", checkresult.NotRun, ""},
 		{"Not defined", "MissingFields", checkresult.NotRun, ""},
 		{"Bad URL", "BadURL", checkresult.Fail, "^Get \"http://invalid/\": dial tcp: lookup invalid:"},
@@ -144,29 +144,29 @@ func TestLibraryPropertiesUrlFieldDeadLink(t *testing.T) {
 		{"Good URL", "Recursive", checkresult.Pass, ""},
 	}
 
-	checkCheckFunction(LibraryPropertiesUrlFieldDeadLink, testTables, t)
+	checkLibraryCheckFunction(LibraryPropertiesUrlFieldDeadLink, testTables, t)
 }
 
 func TestLibraryPropertiesDependsFieldNotInIndex(t *testing.T) {
-	testTables := []checkFunctionTestTable{
+	testTables := []libraryCheckFunctionTestTable{
 		{"Unable to load", "InvalidLibraryProperties", checkresult.NotRun, ""},
 		{"Dependency not in index", "DependsNotIndexed", checkresult.Fail, "^NotIndexed$"},
 		{"Dependency in index", "DependsIndexed", checkresult.Pass, ""},
 		{"No depends", "NoDepends", checkresult.NotRun, ""},
 	}
 
-	checkCheckFunction(LibraryPropertiesDependsFieldNotInIndex, testTables, t)
+	checkLibraryCheckFunction(LibraryPropertiesDependsFieldNotInIndex, testTables, t)
 }
 
 func TestLibraryPropertiesDotALinkageFieldTrueWithFlatLayout(t *testing.T) {
-	testTables := []checkFunctionTestTable{
+	testTables := []libraryCheckFunctionTestTable{
 		{"Unable to load", "InvalidLibraryProperties", checkresult.NotRun, ""},
 		{"Not defined", "MissingFields", checkresult.NotRun, ""},
 		{"Flat layout", "DotALinkageFlat", checkresult.Fail, ""},
 		{"Recursive layout", "DotALinkage", checkresult.Pass, ""},
 	}
 
-	checkCheckFunction(LibraryPropertiesDotALinkageFieldTrueWithFlatLayout, testTables, t)
+	checkLibraryCheckFunction(LibraryPropertiesDotALinkageFieldTrueWithFlatLayout, testTables, t)
 }
 
 func TestLibraryPropertiesIncludesFieldItemNotFound(t *testing.T) {
@@ -181,7 +181,7 @@ func TestLibraryPropertiesIncludesFieldItemNotFound(t *testing.T) {
 }
 
 func TestLibraryPropertiesPrecompiledFieldEnabledWithFlatLayout(t *testing.T) {
-	testTables := []checkFunctionTestTable{
+	testTables := []libraryCheckFunctionTestTable{
 		{"Unable to load", "InvalidLibraryProperties", checkresult.NotRun, ""},
 		{"Not defined", "MissingFields", checkresult.NotRun, ""},
 		{"Flat layout", "PrecompiledFlat", checkresult.Fail, "^true$"},
@@ -190,7 +190,7 @@ func TestLibraryPropertiesPrecompiledFieldEnabledWithFlatLayout(t *testing.T) {
 		{"Flat, not precompiled", "NotPrecompiledFlat", checkresult.NotRun, ""},
 	}
 
-	checkCheckFunction(LibraryPropertiesPrecompiledFieldEnabledWithFlatLayout, testTables, t)
+	checkLibraryCheckFunction(LibraryPropertiesPrecompiledFieldEnabledWithFlatLayout, testTables, t)
 }
 
 func TestLibraryHasSubmodule(t *testing.T) {
