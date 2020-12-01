179329315ce676e9cdb0421fa8eebc951ce8522b
per1234 <accounts@perglass.com>
2020-12-01 02:37:39 -0800

    Increase test coverage of result package

--- result/result_test.go
+++ result/result_test.go
@@ -16,16 +16,229 @@
 package result
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 	"testing"
 
+	"github.com/arduino/arduino-check/check/checkconfigurations"
+	"github.com/arduino/arduino-check/check/checklevel"
+	"github.com/arduino/arduino-check/check/checkresult"
 	"github.com/arduino/arduino-check/configuration"
+	"github.com/arduino/arduino-check/configuration/checkmode"
+	"github.com/arduino/arduino-check/project"
+	"github.com/arduino/arduino-check/project/projecttype"
 	"github.com/arduino/arduino-check/util/test"
 	"github.com/arduino/go-paths-helper"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+func TestInitialize(t *testing.T) {
+	flags := test.ConfigurationFlags()
+	flags.Set("project-type", "sketch")
+	flags.Set("recursive", "false")
+	workingDirectoryPath, err := os.Getwd() // A convenient path that is guaranteed to exist.
+	require.Nil(t, err)
+
+	err = configuration.Initialize(flags, []string{workingDirectoryPath})
+	require.Nil(t, err)
+	var results Type
+	results.Initialize()
+	fmt.Printf("paths: %s", configuration.TargetPaths())
+	assert.Equal(t, paths.NewPathList(workingDirectoryPath), results.Configuration.Paths)
+	assert.Equal(t, projecttype.Sketch.String(), results.Configuration.ProjectType)
+	assert.False(t, results.Configuration.Recursive)
+}
+
+func TestRecord(t *testing.T) {
+	checkedProject := project.Type{
+		Path:             paths.New("/foo/bar"),
+		ProjectType:      projecttype.Sketch,
+		SuperprojectType: projecttype.Library,
+	}
+
+	var results Type
+	checkConfiguration := checkconfigurations.Configurations()[0]
+	checkOutput := "foo"
+	summaryText := results.Record(checkedProject, checkConfiguration, checkresult.Pass, checkOutput)
+	assert.Equal(t, fmt.Sprintf("%s\n", checkresult.Pass), summaryText)
+	summaryText = results.Record(checkedProject, checkConfiguration, checkresult.NotRun, checkOutput)
+	assert.Equal(t, fmt.Sprintf("%s\n%s: %s\n", checkresult.NotRun, checklevel.Notice, checkOutput), summaryText)
+	summaryText = results.Record(checkedProject, checkConfiguration, checkresult.Fail, checkOutput)
+	assert.Equal(t, fmt.Sprintf("%s\n%s: %s\n", checkresult.Fail, checklevel.Error, message(checkConfiguration.MessageTemplate, checkOutput)), summaryText)
+
+	checkResult := checkresult.Pass
+	results = Type{}
+
+	results.Record(checkedProject, checkConfiguration, checkResult, checkOutput)
+	projectReport := results.Projects[0]
+	assert.Equal(t, checkedProject.Path, projectReport.Path)
+	assert.Equal(t, checkedProject.ProjectType.String(), projectReport.ProjectType)
+	projectConfigurationReport := projectReport.Configuration
+	assert.Equal(t, checkmode.Compliance(configuration.CheckModes(checkedProject.ProjectType)), projectConfigurationReport.Compliance)
+	assert.Equal(t, configuration.CheckModes(checkedProject.ProjectType)[checkmode.LibraryManagerSubmission], projectConfigurationReport.LibraryManagerSubmit)
+	assert.Equal(t, configuration.CheckModes(checkedProject.ProjectType)[checkmode.LibraryManagerIndexed], projectConfigurationReport.LibraryManagerUpdate)
+	assert.Equal(t, configuration.CheckModes(checkedProject.ProjectType)[checkmode.Official], projectConfigurationReport.Official)
+
+	checkReport := projectReport.Checks[0]
+	assert.Equal(t, checkConfiguration.Category, checkReport.Category)
+	assert.Equal(t, checkConfiguration.Subcategory, checkReport.Subcategory)
+	assert.Equal(t, checkConfiguration.ID, checkReport.ID)
+	assert.Equal(t, checkConfiguration.Brief, checkReport.Brief)
+	assert.Equal(t, checkConfiguration.Description, checkReport.Description)
+	assert.Equal(t, checkResult.String(), checkReport.Result)
+	checkLevel, _ := checklevel.CheckLevel(checkConfiguration)
+	assert.Equal(t, checkLevel.String(), checkReport.Level)
+	assert.Equal(t, message(checkConfiguration.MessageTemplate, checkOutput), checkReport.Message)
+
+	assert.Len(t, results.Projects, 1)
+	previousProjectPath := checkedProject.Path
+	checkedProject.Path = paths.New("/foo/baz")
+	results.Record(checkedProject, checkConfiguration, checkResult, checkOutput)
+	assert.Len(t, results.Projects, 2)
+
+	assert.Len(t, results.Projects[0].Checks, 1)
+	checkedProject.Path = previousProjectPath
+	results.Record(checkedProject, checkconfigurations.Configurations()[1], checkResult, checkOutput)
+	assert.Len(t, results.Projects[0].Checks, 2)
+}
+
+func TestAddProjectSummary(t *testing.T) {
+	checkedProject := project.Type{
+		Path:             paths.New("/foo/bar"),
+		ProjectType:      projecttype.Sketch,
+		SuperprojectType: projecttype.Library,
+	}
+
+	testTables := []struct {
+		results              []checkresult.Type
+		levels               []checklevel.Type
+		expectedPass         bool
+		expectedWarningCount int
+		expectedErrorCount   int
+	}{
+		{
+			[]checkresult.Type{checkresult.Pass, checkresult.Pass},
+			[]checklevel.Type{checklevel.Info, checklevel.Info},
+			true,
+			0,
+			0,
+		},
+		{
+			[]checkresult.Type{checkresult.Pass, checkresult.Fail},
+			[]checklevel.Type{checklevel.Info, checklevel.Warning},
+			true,
+			1,
+			0,
+		},
+		{
+			[]checkresult.Type{checkresult.Fail, checkresult.Fail},
+			[]checklevel.Type{checklevel.Error, checklevel.Warning},
+			false,
+			1,
+			1,
+		},
+	}
+
+	for _, testTable := range testTables {
+		var results Type
+		for _, result := range testTable.results {
+			results.Record(checkedProject, checkconfigurations.Configurations()[0], result, "")
+		}
+		for checkIndex, level := range testTable.levels {
+			results.Projects[0].Checks[checkIndex].Level = level.String()
+		}
+		results.AddProjectSummary(checkedProject)
+		assert.Equal(t, testTable.expectedPass, results.Projects[0].Summary.Pass)
+		assert.Equal(t, testTable.expectedWarningCount, results.Projects[0].Summary.WarningCount)
+		assert.Equal(t, testTable.expectedErrorCount, results.Projects[0].Summary.ErrorCount)
+		assert.Equal(t, fmt.Sprintf("\nFinished checking project. Results:\nWarning count: %v\nError count: %v\nChecks passed: %v\n\n", testTable.expectedWarningCount, testTable.expectedErrorCount, testTable.expectedPass), results.ProjectSummaryText(checkedProject))
+	}
+}
+
+func TestAddSummary(t *testing.T) {
+	checkedProject := project.Type{
+		Path:             paths.New("/foo/bar"),
+		ProjectType:      projecttype.Sketch,
+		SuperprojectType: projecttype.Library,
+	}
+
+	testTables := []struct {
+		projectSummaries     []summaryReportType
+		expectedPass         bool
+		expectedWarningCount int
+		expectedErrorCount   int
+	}{
+		{
+			[]summaryReportType{
+				{
+					Pass:         true,
+					WarningCount: 0,
+					ErrorCount:   0,
+				},
+				{
+					Pass:         true,
+					WarningCount: 0,
+					ErrorCount:   0,
+				},
+			},
+			true,
+			0,
+			0,
+		},
+		{
+			[]summaryReportType{
+				{
+					Pass:         true,
+					WarningCount: 1,
+					ErrorCount:   0,
+				},
+				{
+					Pass:         true,
+					WarningCount: 2,
+					ErrorCount:   0,
+				},
+			},
+			true,
+			3,
+			0,
+		},
+		{
+			[]summaryReportType{
+				{
+					Pass:         false,
+					WarningCount: 1,
+					ErrorCount:   0,
+				},
+				{
+					Pass:         true,
+					WarningCount: 2,
+					ErrorCount:   2,
+				},
+			},
+			false,
+			3,
+			2,
+		},
+	}
+
+	for _, testTable := range testTables {
+		var results Type
+		for projectIndex, projectSummary := range testTable.projectSummaries {
+			checkedProject.Path = paths.New(fmt.Sprintf("/foo/bar%v", projectIndex)) // Use a unique path to generate a new project report.
+			results.Record(checkedProject, checkconfigurations.Configurations()[0], checkresult.Pass, "")
+			results.AddProjectSummary(checkedProject)
+			results.Projects[projectIndex].Summary = projectSummary
+		}
+		results.AddSummary()
+		assert.Equal(t, testTable.expectedPass, results.Summary.Pass)
+		assert.Equal(t, testTable.expectedPass, results.Passed())
+		assert.Equal(t, testTable.expectedWarningCount, results.Summary.WarningCount)
+		assert.Equal(t, testTable.expectedErrorCount, results.Summary.ErrorCount)
+		assert.Equal(t, fmt.Sprintf("Finished checking projects. Results:\nWarning count: %v\nError count: %v\nChecks passed: %v\n", testTable.expectedWarningCount, testTable.expectedErrorCount, testTable.expectedPass), results.SummaryText())
+	}
+}
 
 func TestWriteReport(t *testing.T) {
 	flags := test.ConfigurationFlags()
