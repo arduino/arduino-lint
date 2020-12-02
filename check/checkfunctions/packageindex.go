b592a1f5be5ddeb78f144cb31a0e365a6f084a5c
per1234 <accounts@perglass.com>
2020-12-01 20:49:32 -0800

    Add check for invalid package index JSON format

---
+++ check/checkfunctions/packageindex.go
@@ -0,0 +1,33 @@
+// This file is part of arduino-check.
+//
+// Copyright 2020 ARDUINO SA (http://www.arduino.cc/)
+//
+// This software is released under the GNU General Public License version 3,
+// which covers the main part of arduino-check.
+// The terms of this license can be found at:
+// https://www.gnu.org/licenses/gpl-3.0.en.html
+//
+// You can be released from the requirements of the above licenses by purchasing
+// a commercial license. Buying such a license is mandatory if you want to
+// modify or otherwise use the software for commercial activities involving the
+// Arduino software without disclosing the source code of your own applications.
+// To purchase a commercial license, send an email to license@arduino.cc.
+
+package checkfunctions
+
+import (
+	"github.com/arduino/arduino-check/check/checkdata"
+	"github.com/arduino/arduino-check/check/checkresult"
+)
+
+// The check functions for package indexes.
+
+// PackageIndexJSONFormat checks whether the package index file is a valid JSON document.
+func PackageIndexJSONFormat() (result checkresult.Type, output string) {
+	if isValidJSON(checkdata.ProjectPath()) {
+		return checkresult.Pass, ""
+	}
+
+	return checkresult.Fail, ""
+}
+
