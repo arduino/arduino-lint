c260881b4179ec564cb28f5d99f6321dbc3bb155
per1234 <accounts@perglass.com>
2020-12-10 03:49:30 -0800

    Make --recursive flag a string input
    Previously, the `--recursive` flag had a Boolean type. That is logical, since it uses values of true and false. However,
    the fact that the default value of this flag is true changes matters. With the other Boolean flags, the default value
    is false, so when you want to set it to true you only need to use the flag without a value, as is standard. But with a
    default true flag it's necessary to unset the value, which can't be done with a bare flag, so you must pass false to the
    flag. You would expect to be able to do this with either a space separator or an equals sign, just like
    with the string flags, but the former is not supported:
    
    $ ./arduino-check --recursive false
    Configuration error: PROJECT_PATH argument false does not exist
    
    Only the equals sign is supported (`--recursive=false`).
    
    So I think the UI is more intuitive with this flag a string type, so the behavior is consistent with the other flags.

--- cli/cli.go
+++ cli/cli.go
@@ -35,7 +35,7 @@ func Root() *cobra.Command {
 	rootCommand.PersistentFlags().String("format", "text", "The output format can be {text|json}.")
 	rootCommand.PersistentFlags().String("library-manager", "", "Configure the checks for libraries in the Arduino Library Manager index. Can be {submit|update|false}.\nsubmit: Also run additional checks required to pass before a library is accepted for inclusion in the index.\nupdate: Also run additional checks required to pass before new releases of a library already in the index are accepted.\nfalse: Don't run any Library Manager-specific checks.")
 	rootCommand.PersistentFlags().String("project-type", "all", "Only check projects of the specified type and their subprojects. Can be {sketch|library|all}.")
-	rootCommand.PersistentFlags().Bool("recursive", true, "Search path recursively for Arduino projects to check. Can be {true|false}.")
+	rootCommand.PersistentFlags().String("recursive", "true", "Search path recursively for Arduino projects to check. Can be {true|false}.")
 	rootCommand.PersistentFlags().String("report-file", "", "Save a report on the checks to this file.")
 	rootCommand.PersistentFlags().BoolP("verbose", "v", false, "Show more information while running checks.")
 	rootCommand.PersistentFlags().Bool("version", false, "Print version and timestamp of the build.")
