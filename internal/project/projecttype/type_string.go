28c1b1b6cd1e911217751b218c18e7ebe9c6d0ca
per1234 <accounts@perglass.com>
2025-10-30 04:29:24 -0700

    Update generated code to reflect `golang.org/x/tools` bump
    The new version produces different stringer code than the previous version.

--- internal/project/projecttype/type_string.go
+++ internal/project/projecttype/type_string.go
@@ -21,8 +21,9 @@ const _Type_name = "sketchlibraryplatformpackage-indexallN/A"
 var _Type_index = [...]uint8{0, 6, 13, 21, 34, 37, 40}
 
 func (i Type) String() string {
-	if i < 0 || i >= Type(len(_Type_index)-1) {
+	idx := int(i) - 0
+	if i < 0 || idx >= len(_Type_index)-1 {
 		return "Type(" + strconv.FormatInt(int64(i), 10) + ")"
 	}
-	return _Type_name[_Type_index[i]:_Type_index[i+1]]
+	return _Type_name[_Type_index[idx]:_Type_index[idx+1]]
 }
